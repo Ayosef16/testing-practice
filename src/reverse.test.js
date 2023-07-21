2adf359fa7826f873f960f1a14dfcef8c431da3e
Ayosef <syrivera@uc.cl>
2023-07-21 14:59:34 -0400

    Add testing for reverse string

--- src/reverse.test.js
+++ src/reverse.test.js
@@ -3,3 +3,9 @@ import { reverseString } from "./reverse";
 test("reverse a string with 1 word", () => {
   expect(reverseString("trying")).toBe("gniyrt");
 });
+
+test("reverse a string with more than 1 word", () => {
+  expect(reverseString("PaBlo Clavo un CLAVITO")).toBe(
+    "OTIVALC nu ovalC olBaP"
+  );
+});
